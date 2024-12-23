df7ad4cfe7f1cf0e012b978254f12790675038a7
KhalilJaber97 <khalil.jaber19972020@gmail.com>
2024-12-23 02:39:01 +0200

    init

---
+++ Dead by daylight.js
@@ -0,0 +1,9 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Как купить ДБД в России в 2024 году? Перейдите на GGSel, выберите категорию «Steam» и найдите услугу пополнения кошелька или покупки ключа/подарка.'
+
+});
